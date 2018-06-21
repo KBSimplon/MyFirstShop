47c172a9c8ac94c00dda3dfd37c59ef0410a6864
Kévin Beauguet <k.beauguet@gmail.com>
2018-06-21 18:01:15 +0200

    Project Initialization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  createdItem = new Date();
+  items = [
+    {
+      name: 'Potion',
+      content: 'A potion which restores 50 HP',
+      price: '25$',
+      in_stock: 5,
+      created_at: this.createdItem
+    },
+    {
+      name: 'Elixir',
+      content: 'An elixir which restores 25 MP',
+      price: '50$',
+      in_stock: 3,
+      created_at: new Date()
+    },
+    {
+      name: 'Antidote',
+      content: 'An antidote which cures the poison',
+      price: '75$',
+      in_stock: 0,
+      created_at: new Date()
+    },
+    {
+      name: 'Sword',
+      content: 'A great weapon to kick some asss',
+      price: '150$',
+      in_stock: 1,
+      created_at: new Date()
+    },
+    {
+      name: 'Developer Plush',
+      content: 'It\'s awesome, It\'s wonderfull, It\'s FREE !!!!',
+      price: 'FREE',
+      in_stock: 0,
+      created_at: new Date()
+    },
+    {
+      name: 'Old Woman',
+      content: 'She is back ! Take care of the explosion ...',
+      price: '2000$',
+      in_stock: 0,
+      created_at: new Date()
+    }
+  ]
 }
---
+++ src/app/post-list/post-list.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.scss']
+})
+export class PostListComponent implements OnInit {
+
+  @Input() itemName: string;
+  @Input() itemContent: string;
+  @Input() itemPrice: string;
+  @Input() itemStock: number;
+  @Input() itemDate: Date;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  buyItem() {
+    this.itemStock--;
+    if(this.itemStock === 0) {
+      alert("Thank you, this was the last " + this.itemName + " in stock.")
+    }
+  }
+
+  sellItem() {
+    this.itemStock++;
+  }
+
+/*   getColor() {
+    if(this.itemStock === 0) {
+      return 'red'; 
+    } else {
+      return 'lightgreen';
+    }
+  } */
+
+}
